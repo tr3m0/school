80bc44b01e78d87e0fb55f4ad3091fb46c627fb8
Leonardo Tremolada <leonardo.tremolada@gmail.com>
2025-04-09 17:36:49 +0200

    rsa

--- 5a/crittografia/rsa/rsa.py
+++ 5a/crittografia/rsa/rsa.py
@@ -1,39 +1,42 @@
-import json
-from base64 import b64encode, b64decode
 from crypt import *
 
 class Generator:
     def __call__(self):
         pass
 
-class PrimeGenerator(Generator):
-    def __init__(self, **kwargs):
-        self.primes = []
-        self.prime_checker = kwargs["checker"]
-        self.interval = kwargs["interval"]
-
-    def __call__(self):
-        for n in self.interval():
-            if self.prime_checker(n) and n not in self.primes:
-                self.primes.append(n)
-                return n
-        return None
-
-class IntervalByDigitsGenerator(Generator):
+class IntervalByDigits:
     def __init__(self, digits):
         self.digits = digits
 
-    def __call__(self):
-        return range(self.min(), self.max())
+    def __iter__(self):
+        i = self.min()
+        while i <= self.max():
+            yield i
+            i += 1
+
     def min(self):
         return 10 ** (self.digits - 1)
 
     def max(self):
         return (10 ** self.digits) - 1
 
+class PrimeGenerator(Generator):
+    def __init__(self, checker, interval):
+        self.generated = []
+        self.is_prime = checker
+        self.interval = interval
+
+    def __call__(self):
+        for n in self.interval:
+            if self.is_prime(n) and n not in self.generated:
+                self.generated.append(n)
+                return n
+        return None
+
 class KeysGenerator(Generator):
-    def __init__(self, **kwargs):
-        self.prime_generator = kwargs["prime_generator"]
+    def __init__(self, prime_generator):
+        self.prime_generator = prime_generator
+
     def __call__(self):
         p = self.prime_generator()
         q = self.prime_generator()
@@ -41,52 +44,54 @@ class KeysGenerator(Generator):
         phi = (p - 1) * (q - 1)
         e = self._find_e(phi)
         d = pow(e, -1, phi)
-        return {
-            "public": (e, n),
-            "private": (d, n),
-            "p": p,
-            "q": q
-        }
+
+        return [
+            (e, n), # public
+            (d, n)  # private
+        ]
 
     def _find_e(self, phi):
-        for n in range(2, phi):
-            if mcd(n, phi) == 1:
-                return n
-        return None
+        i = 2
+        while True:
+            if mcd(i, phi) == 1:
+                return i
+            else:
+                i += 1
 
-def cipher(cleartext, key):
+def encrypt(cleartext, public_key):
+    (e, n) = public_key
     cyphertext = []
     for c in cleartext:
         M = ord(c)
-        C = pow(M, key[0], key[1])
+        C = pow(M, e, n)
         cyphertext.append(C)
-    return b64encode(json.dumps(cyphertext).encode("utf-8"))
+    return cyphertext
 
-def decipher(cyphertext, key):
-    cyphertext = json.loads(b64decode(cyphertext).decode("utf-8"))
+def decrypt(cyphertext, private_key):
+    (d, n) = private_key
     cleartext = ""
     for C in cyphertext:
-        M = pow(C, key[0], key[1])
+        M = pow(C, d, n)
         cleartext += chr(M)
     return cleartext
 
-digits = 0
-while digits < 7:
-    digits = int(input("Insert the number of digits (min 7): "))
+digits_count = 0
+while digits_count < 7:
+    digits_count = int(input("Insert the number of digits (min 7): "))
 
 keys_generator = KeysGenerator(
     prime_generator=PrimeGenerator(
         checker=isPrimeTrivial,
-        interval=IntervalByDigitsGenerator(digits)
+        interval=IntervalByDigits(digits_count)
     )
 )
-keys = keys_generator()
+[public_key, private_key] = keys_generator()
 
-print(f"Public key = {keys["public"]}")
-print(f"Private key = {keys["private"]}")
-print(f"p = {keys["p"]}")
-print(f"q = {keys["q"]}")
-cleartext = input("Insert the text to be encrypted: ")
-cyphertext = cipher(cleartext, keys["public"])
-print(f"Cyphertext = {cyphertext}")
-print(f"Cleartext = {decipher(cyphertext, keys["private"])}")
+print(f"{public_key=}")
+print(f"{private_key=}")
+
+message = input("Insert the text to be encrypted: ")
+cyphertext = encrypt(message, public_key)
+cleartext = decrypt(cyphertext, private_key)
+print(f"{cyphertext=}")
+print(f"{cleartext=}")
